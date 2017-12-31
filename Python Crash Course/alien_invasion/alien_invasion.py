dffbdc4c02d883fd144ac6c1b3f1fe14e992c00d
flywi <flywi@icloud.com>
2017-12-31 18:57:11 +0800

    make file format accordance to PEP8, button text display problem remained.

--- Python Crash Course/alien_invasion/alien_invasion.py
+++ Python Crash Course/alien_invasion/alien_invasion.py
@@ -8,6 +8,7 @@ import game_function as gf
 from game_stats import GameStats
 from button import Button
 
+
 def run_game():
     # initiate the game and create a screen object
     pygame.init()
@@ -15,18 +16,18 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width,
                                       ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
-    #create a "Play" button
+    # create a "Play" button
     play_button = Button(ai_settings, screen, 'Play')
-    #create an instance to store statistics information of the game
+    # create an instance to store statistics information of the game
     stats = GameStats(ai_settings)
 
-    #create a ship
+    # create a ship
     ship = Ship(ai_settings, screen)
-    #create a group to store the bullets
+    # create a group to store the bullets
     bullets = Group()
-    #create a group to store the aliens
+    # create a group to store the aliens
     aliens = Group()
-    #create aliens
+    # create aliens
     gf.create_fleet(ai_settings, screen, ship, aliens)
     while True:
 
@@ -38,5 +39,6 @@ def run_game():
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
             gf.update_screen(ai_settings, screen, ship, aliens, bullets,
                              play_button)
+
 
 run_game()
--- Python Crash Course/alien_invasion/ship.py
+++ Python Crash Course/alien_invasion/ship.py
@@ -1,41 +1,42 @@
 import pygame
 
-class Ship():
-    def __init__(self, ai_settings, screen):
-        '''Initiate the ship and set the initial position'''
-        self.screen = screen
-        self.ai_settings=ai_settings
 
-        #Load the spaceship image and get the outter rectagle
+class Ship:
+    def __init__(self, ai_settings, screen):
+        """Initiate the ship and set the initial position"""
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        # Load the spaceship image and get the outer rectangle
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
-        #put the new ship in the botton center of the screen
+        # put the new ship in the bottom center of the screen
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
-        #store float value in ship attitude center
+        # store float value in ship attitude center
         self.center = float(self.rect.centerx)
 
-        #moving tag
+        # moving tag
         self.moving_right = False
         self.moving_left = False
 
     def blitme(self):
-        '''draw the ship on assigned position'''
+        """draw the ship on assigned position"""
         self.screen.blit(self.image, self.rect)
 
     def update(self):
-        '''adjust ship position according to moving tag'''
+        """adjust ship position according to moving tag"""
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
         if self.moving_left and self.rect.left > 0:
-            self.center -=  self.ai_settings.ship_speed_factor
+            self.center -= self.ai_settings.ship_speed_factor
 
-        #update rect according to self.center
+        # update rect according to self.center
         self.rect.centerx = self.center
 
     def center_ship(self):
-        '''put the ship in the center bottom of the screen'''
+        """put the ship in the center bottom of the screen"""
         self.center = self.screen_rect.centerx
