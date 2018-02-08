8c451474b5f5a82539e43f73648af3074514d420
flywi <flywi@icloud.com>
2018-02-08 12:55:19 +0800

    finishe ch3 of MLiA

---
+++ MLiA/ch03/trees.py
@@ -0,0 +1,111 @@
+from math import log
+import operator
+import pickle
+
+
+def calcShannonEnt(dataset):
+    numEntries = len(dataset)
+    labelCounts = {}
+    for featVec in dataset:
+        currentLabel =featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key]) / numEntries
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
+def createDataSet():
+    dataset = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 0, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataset, labels
+
+
+def splitDataSet(dataset, axis, value):
+    retDataSet = []
+    for featVec in dataset:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis + 1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeature = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeature):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if infoGain > bestInfoGain:
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+
+def majorityCnt(classList):
+    classCnt = {}
+    for vote in classList:
+        if vote not in classCnt.keys():
+            classCnt[vote] = 0
+        classCnt[vote] += 1
+    sortedClassCnt = sorted(classCnt.items(), key = operator.itemgetter(1),
+            reverse = True)
+    return sortedClassCnt[0][0]
+
+
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel: {}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet,
+                                     bestFeat,value), subLabels)
+    return myTree
+
+
+def classify(inputTree, featLabels, testVec):
+    firstStr = list(inputTree.keys())[0]
+    secondDict = inputTree[firstStr]
+    featIndex = featLabels.index(firstStr)
+    for key in secondDict.keys():
+        if testVec[featIndex] == key:
+            if type(secondDict[key]) == dict:
+                classLabel = classify(secondDict[key], featLabels, testVec)
+            else:
+                classLabel = secondDict[key]
+    return classLabel
+
+
+def storeTree(inputTree, filename):
+    fw = open(filename, 'wb')
+    pickle.dump(inputTree, fw)
+    fw.close()
+    
+    
+def grabTree(filename):
+    fr = open(filename, 'rb')
+    return pickle.load(fr)
